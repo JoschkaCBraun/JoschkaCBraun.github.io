86fdb7fc8aad831f2623be1b227b28445838e67c
Joschka <joschkacbraun@gmail.com>
2025-11-17 12:11:21 +0100

    add NeurIPS 2025 Workshop paper on Exploration Hacking

--- build.py
+++ build.py
@@ -19,7 +19,7 @@ def get_personal_data() -> Tuple[List[str], str, str, str]:
                 <p>
                     Currently, I'm a scholar in the <a href="https://www.matsprogram.org/" target="_blank">ML Alignment & Theory Scholars (MATS) Program</a>, working on Scalable Oversight and AI Control with 
                     <a href="https://scholar.google.com/citations?user=p_aH5fgAAAAJ&hl=en" target="_blank">David Lindner</a>, 
-                    <a href="https://scholar.google.com/citations?user=8DgF8HcAAAAJ&hl=de" target="_blank">Erik Jenner</a>, and 
+                    <a href="https://rzimmermann.com/" target="_blank">Roland Zimmermann</a>, and 
                     <a href="https://scholar.google.com/citations?user=LoT0z6oAAAAJ&hl=en" target="_blank">Scott Emmons</a>.
                 </p>
                 <p>
